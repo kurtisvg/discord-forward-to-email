d4df54fadc1589277face173a288e44d596e817a
kpvangent <31518063+kurtisvg@users.noreply.github.com>
2026-04-05 13:55:25 -0600

    refactor: split flags and options into flags.go

--- cmd/root.go
+++ cmd/root.go
@@ -1,55 +1,9 @@
 package cmd
 
 import (
-	"flag"
 	"fmt"
-	"log/slog"
 	"os"
 )
-
-type options struct {
-	register bool
-	port     string
-
-	discordToken     string
-	discordAppID     string
-	discordPublicKey string
-
-	gmailUser        string
-	gmailAppPassword string
-}
-
-func parseFlags(args []string) options {
-	var opts options
-	fs := flag.NewFlagSet("discord-forward-to-email", flag.ExitOnError)
-	fs.BoolVar(&opts.register, "register", false, "Register the Discord message command and exit")
-	fs.StringVar(&opts.port, "port", envOrDefault("PORT", "8080"), "HTTP server port")
-	fs.StringVar(&opts.discordToken, "discord-token", os.Getenv("DISCORD_TOKEN"), "Discord bot token")
-	fs.StringVar(&opts.discordAppID, "discord-app-id", os.Getenv("DISCORD_APP_ID"), "Discord application ID")
-	fs.StringVar(&opts.discordPublicKey, "discord-public-key", os.Getenv("DISCORD_PUBLIC_KEY"), "Discord public key for signature verification")
-	fs.StringVar(&opts.gmailUser, "gmail-user", os.Getenv("GMAIL_USER"), "Gmail address")
-	fs.StringVar(&opts.gmailAppPassword, "gmail-app-password", os.Getenv("GMAIL_APP_PASSWORD"), "Gmail app password")
-	if err := fs.Parse(args); err != nil {
-		os.Exit(1)
-	}
-	return opts
-}
-
-func (o options) require(flags ...string) {
-	vals := map[string]string{
-		"discord-token":      o.discordToken,
-		"discord-app-id":     o.discordAppID,
-		"discord-public-key": o.discordPublicKey,
-		"gmail-user":         o.gmailUser,
-		"gmail-app-password": o.gmailAppPassword,
-	}
-	for _, name := range flags {
-		if vals[name] == "" {
-			slog.Error("required config is not set", "flag", name)
-			os.Exit(1)
-		}
-	}
-}
 
 func Execute() {
 	opts := parseFlags(os.Args[1:])
@@ -62,11 +16,4 @@ func Execute() {
 
 	_ = opts // will be used when server is implemented
 	fmt.Println("server not yet implemented")
-}
-
-func envOrDefault(key, fallback string) string {
-	if v := os.Getenv(key); v != "" {
-		return v
-	}
-	return fallback
 }
